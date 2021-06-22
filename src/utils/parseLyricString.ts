5c4619692541774c14746619d7ab2736ce33e68e
pacexy <pacexy@gmail.com>
2021-06-22 15:21:17 +0800

    Fix lyric parser for multi-timestamp case

--- src/utils/parseLyricString.ts
+++ src/utils/parseLyricString.ts
@@ -1,32 +1,56 @@
 import dayjs from 'dayjs'
 import { compact } from 'lodash'
 
+interface RawParsedLyric {
+  begin: number
+  content: string | undefined
+}
+
+// https://stackoverflow.com/a/34789675/13151903
+// https://regex101.com/r/WToTPn/1
+const regex =
+  /(?:\[(?<m>\d+):(?<s>\d+)\.(?<ms>\d+)\])?(?:(?=\[\d+:\d+\.\d+\])|(?<content>.*))/gs
+
 export function parseLyricString(rawLyric: string) {
   const lyric = compact(rawLyric.split('\n'))
-  const parsedLyric = lyric
-    ?.map((rawSentence) => {
-      const matches = rawSentence.match(
-        /(\[(?<minute>\d+):(?<second>\d+).(?<millisecond>\d+)\])?(?<content>.*)/,
-      )
-      return {
-        begin: dayjs
-          .duration({
-            minutes: matches?.groups?.minute,
-            seconds: matches?.groups?.second,
-            milliseconds: matches?.groups?.millisecond,
-          })
-          .asMilliseconds(),
-        content: matches?.groups?.content,
-      }
-    })
+  const rawParsedLyric: RawParsedLyric[] = []
+
+  lyric?.forEach((rawSentence) => {
+    const matches = rawSentence.matchAll(regex)
+    let content: string
+    const _rawParsedLyric: RawParsedLyric[] = []
+
+    Array.from(matches)
+      .reverse()
+      .forEach((match) => {
+        const _content = match?.groups?.content
+        if (_content) {
+          content = _content
+        }
+
+        _rawParsedLyric.push({
+          begin: dayjs
+            .duration({
+              minutes: Number(match?.groups?.m),
+              seconds: Number(match?.groups?.s),
+              milliseconds: Number(match?.groups?.ms),
+            })
+            .asMilliseconds(),
+          content,
+        })
+      })
+
+    rawParsedLyric.push(..._rawParsedLyric)
+  })
+
+  return rawParsedLyric
     .sort((sentence1, sentence2) => sentence1.begin - sentence2.begin)
-    .map((sentence, index, parsedLyric) => {
-      const nextSentence = parsedLyric[index + 1]
+    .map((sentence, index, rawParsedLyric) => {
+      const nextSentence = rawParsedLyric[index + 1]
       return {
         ...sentence,
         duration: nextSentence ? nextSentence.begin - sentence.begin : 0,
       }
     })
-
-  return parsedLyric.filter((sentence) => sentence.content)
+    .filter((sentence) => sentence.content)
 }
